fcbc2d0ec9991a11bddeef4b802c8f7bed952c84
Josipa006 <josipa.bevanda@fpmoz.sum.ba>
2026-06-12 00:10:44 +0200

    Dodana zadaca 8

---
+++ zadaca8.py
@@ -0,0 +1,8 @@
+"""
+Funkciju iz prethodne zadaće učitati kao funkciju modula u novi program i pozvati je nakon traženja unosa od korisnika.
+"""
+from modul import ispis_unatrag
+
+tekst = input("Unesite tekst: ")
+
+ispis_unatrag(tekst)
