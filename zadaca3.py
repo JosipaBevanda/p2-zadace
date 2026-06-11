6780f5324f39aef504c12bf727280108ee821231
Josipa006 <josipa.bevanda@fpmoz.sum.ba>
2026-06-11 23:55:46 +0200

    initial commit

--- zadaca3.py
+++ zadaca3.py
@@ -1,24 +1,53 @@
 """
-Napisati regex za provjeru validnosti unosa e-maila. E-Mail mora biti formata ime.prezime@fpmoz.sum.ba
-Nakon toga napisati regex za provjeru eduId koji mora biti formata ime.prezimeX@sum.ba 
-X predstavlja bilo koji broj (moze ici u beskonacnost), a taj broj ne mora postojati (može biti samo ime.prezime@sum.ba).
-Od korisnika zatražiti unos maila i eduid te ispisati uspješnost.
+Iz podataka učitanih u prethodnom primjeru sortirati listu po prezimenima.
+Napraviti novi rječnik gdje će se po bodovnom rangu upisivati broj ostvarenih ocjena. 
+Nedovoljan
+0-49%
+Dovoljan
+50-65%
+Dobar
+65-80%
+Vrlodobar
+80-90%
+Izvrstan
+90-100%
 """
+imena = ["Ivan", "Ana", "Marko"]
+prezimena = ["Ivic", "Anic", "Markic"]
+godine = [20, 19, 21]
+postotci = [45, 78, 92]
 
-import re
+studenti = list(zip(imena, prezimena, godine, postotci))
 
-email_regex = r"^[a-zA-Z]+\.[a-zA-Z]+@fpmoz\.sum\.ba$"
-eduid_regex = r"^[a-zA-Z]+\.[a-zA-Z]+[0-9]*@sum\.ba$"
+# sortiranje po prezimenu (drugi element tuplea)
+studenti.sort(key=lambda student: student[1])
 
-email = input("Unesi e-mail: ")
-eduid = input("Unesi eduId: ")
+print("Sortirana lista po prezimenima:")
+for student in studenti:
+    print(student)
 
-if re.match(email_regex, email):
-    print("E-mail je ispravan.")
-else:
-    print("E-mail nije ispravan.")
+rangovi = {
+    "Nedovoljan": 0,
+    "Dovoljan": 0,
+    "Dobar": 0,
+    "Vrlodobar": 0,
+    "Izvrstan": 0
+}
 
-if re.match(eduid_regex, eduid):
-    print("eduId je ispravan.")
-else:
-    print("eduId nije ispravan.")
+for student in studenti:
+    postotak = student[3]
+
+    if postotak < 50:
+        rangovi["Nedovoljan"] += 1
+    elif postotak <= 65:
+        rangovi["Dovoljan"] += 1
+    elif postotak <= 80:
+        rangovi["Dobar"] += 1
+    elif postotak <= 90:
+        rangovi["Vrlodobar"] += 1
+    else:
+        rangovi["Izvrstan"] += 1
+
+print("\nBroj ostvarenih ocjena:")
+for rang in rangovi:
+    print(rang, ":", rangovi[rang])
